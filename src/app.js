36b6a6ffe5d1272f3f1d2af8e94cc6bc7aec04eb
kushaLG0D <neranikushal18@gmail.com>
2022-02-03 01:27:39 +0530

    Change Path Using Path-Module

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 const express = require("express");
+const path=require("path");
 const app = express();
 const hbs = require("hbs");
-const publicPath = `C:/Users/neran/Desktop/WebDevelopment/Nodejs/Project2/public`;
 const port = process.env.PORT || 3000;
-const viewsPath = `C:/Users/neran/Desktop/WebDevelopment/Nodejs/Project2/templates/views`;
-const partialsPath = `C:/Users/neran/Desktop/WebDevelopment/Nodejs/Project2/templates/partials`;
+
 //public static path
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, '../public')));
 
 //template engine
+app.set("views", path.join(__dirname, '../templates/views'));
 app.set("view engine", "hbs");
-app.set("views", viewsPath);
-hbs.registerPartials(partialsPath);
+hbs.registerPartials(path.join(__dirname, '../templates/partials'));
 
 app.get("/", (req, res) => {
   res.render("index");
